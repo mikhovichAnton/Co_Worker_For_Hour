9b2319983e13543c26261a376f30718a75ac6a20
Mikhovich Anton <mikhovich.anton@gmail.com>
2023-08-11 15:59:53 +0300

    1st set

--- app/src/main/java/com/android/coworkerforhour/SplashScreenActivity.kt
+++ app/src/main/java/com/android/coworkerforhour/SplashScreenActivity.kt
@@ -1,7 +1,8 @@
 package com.android.coworkerforhour
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.coworkerforhour.databinding.ActivitySplashScreenBinding
 
@@ -9,11 +10,12 @@ class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        Thread.sleep(3000L)
         startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
         finish()
     }
